1610277dada5c953730a6e9234de815472196349
alkeshnagar56 <alkeshnagar56@gmail.com>
2024-11-29 13:16:44 +0530

    portfolio update

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -14,7 +14,7 @@ import Logo from "../components/common/logo";
 import Footer from "../components/common/footer";
 import NavBar from "../components/common/navBar";
 import Article from "../components/homepage/article";
-import Works from "../components/homepage/works";
+// import Works from "../components/homepage/works";
 import AllProjects from "../components/projects/allProjects";
 
 import INFO from "../data/user";
